c7334bac4a3040f218b2d208f08eaa188477c24c
Eduardo Queiroz <eduhmq@gmail.com>
2024-12-11 23:31:28 -0300

    get countries data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,70 @@
-// import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 import { Card, Header, Footer } from "./components";
-const countries = [
-  {
-    id: 1,
-    country: "Brazil",
-    capital: "Brasília",
-    region: "South America",
-    population: 213993437, // Approximate population
-  },
-  {
-    id: 2,
-    country: "Japan",
-    capital: "Tokyo",
-    region: "Asia",
-    population: 125800000, // Approximate population
-  },
-  {
-    id: 3,
-    country: "Germany",
-    capital: "Berlin",
-    region: "Europe",
-    population: 83783942, // Approximate population
-  },
-  {
-    id: 4,
-    country: "Australia",
-    capital: "Canberra",
-    region: "Oceania",
-    population: 25788217, // Approximate population
-  },
-  {
-    id: 5,
-    country: "Canada",
-    capital: "Ottawa",
-    region: "North America",
-    population: 38246108, // Approximate population
-  },
-  {
-    id: 6,
-    country: "India",
-    capital: "New Delhi",
-    region: "Asia",
-    population: 1406638500, // Approximate population
-  },
-  {
-    id: 7,
-    country: "South Africa",
-    capital: "Pretoria",
-    region: "Africa",
-    population: 60041996, // Approximate population
-  },
-  {
-    id: 8,
-    country: "Mexico",
-    capital: "Mexico City",
-    region: "North America",
-    population: 126705138, // Approximate population
-  },
-];
+
+type Country = {
+  cca3: string;
+  flags: {
+    svg: string;
+  };
+  name: {
+    common: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+};
 
 export default function Home() {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(
+          "https://restcountries.com/v3.1/all?fields=cca3,flags,name,capital,region,population"
+        );
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        setError("Failed to fetch data");
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
   return (
     <>
       <Header />
       <main className="flex-1">
         <div className="grid grdii-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {countries.map(({ id, country, capital, region, population }) => (
-            <Card
-              key={id}
-              country={country}
-              capital={capital}
-              region={region}
-              population={population}
-            />
-          ))}
+          {countries.map(
+            ({ cca3, flags, name, capital, region, population }, index) => {
+              const { svg: flag } = flags ?? {};
+              const { common: countryName } = name ?? {};
+              const [capitalName] = capital ?? [];
+
+              return (
+                <Card
+                  key={cca3}
+                  index={index}
+                  flag={flag}
+                  name={countryName}
+                  capital={capitalName}
+                  region={region}
+                  population={population}
+                />
+              );
+            }
+          )}
         </div>
       </main>
       <Footer />
